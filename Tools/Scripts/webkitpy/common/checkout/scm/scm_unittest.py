11807c13227b6c5d57c127315f1dd85297e333a4
Sam Sneddon <gsnedders@apple.com>
2024-10-21 10:58:39 -0700

    Garden webkitpy.common.checkout.scm.scm_unittest failures https://bugs.webkit.org/show_bug.cgi?id=277309
    Reviewed by NOBODY (OOPS!).
    
    * Tools/Scripts/webkitpy/common/checkout/scm/scm_unittest.py:

--- Tools/Scripts/webkitpy/common/checkout/scm/scm_unittest.py
+++ Tools/Scripts/webkitpy/common/checkout/scm/scm_unittest.py
@@ -632,12 +632,15 @@ MOCK run_command: ['git', 'log', '-1', '--grep=git-svn-id:', '--date=iso', './MO
     def test_push_local_commits_to_server_with_username_and_password(self):
         self.assertEqual(self.make_scm().push_local_commits_to_server(username='dbates@webkit.org', password='blah'), "MOCK output of child process")
 
+    @xfail(reason="https://bugs.webkit.org/show_bug.cgi?id=277309")
     def test_push_local_commits_to_server_without_username_and_password(self):
         self.assertRaises(AuthenticationError, self.make_scm().push_local_commits_to_server)
 
+    @xfail(reason="https://bugs.webkit.org/show_bug.cgi?id=277309")
     def test_push_local_commits_to_server_with_username_and_without_password(self):
         self.assertRaises(AuthenticationError, self.make_scm().push_local_commits_to_server, {'username': 'dbates@webkit.org'})
 
+    @xfail(reason="https://bugs.webkit.org/show_bug.cgi?id=277309")
     def test_push_local_commits_to_server_without_username_and_with_password(self):
         self.assertRaises(AuthenticationError, self.make_scm().push_local_commits_to_server, {'password': 'blah'})
 
